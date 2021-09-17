8c18bc3e7657690797c980729aefbb840ed0272d
MicroHub69 <87417063+MicroHub69@users.noreply.github.com>
2021-09-17 16:51:49 +0100

    F-Push

---
+++ assets/js/navigation.js
@@ -0,0 +1,24 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".form");
+    const navLinks = document.querySelectorAll(".form li");
+
+    //toggle nav
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("burger-active");
+
+        //Animation Links
+        navLinks.forEach((link, index) =>{
+            if(link.style.animation){
+                link.style.animation = "";
+            } else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 9 + 0.5}s`;
+            }
+        });
+
+        //bURGER aNIMATION
+        burger.classList.toggle("toggle");
+    });
+}
+
+navSlide()
