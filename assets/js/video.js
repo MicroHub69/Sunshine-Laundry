ff8d26dfe00d61cb9a3e432d900580ef880baa8f
MicroHub69 <87417063+MicroHub69@users.noreply.github.com>
2021-10-04 09:14:59 +0100

    f-commit

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,18 +1,4 @@
 $(document).ready(function(){
-    $(".first-question").click(function(){
-        $(".first-answer").toggle(500)
-    })
-    $(".first-question1").click(function(){
-        $(".first-answer1").toggle(500)
-    })
-    $(".first-question2").click(function(){
-        $(".first-answer2").toggle(500)
-    })
-    $(".first-question3").click(function(){
-        $(".first-answer3").toggle(500)
-    })//////FAQ TOGGLE CLASS
-
-
 
     $(".fancy-button").click(function(){
         $(".toggle-on-click").show(function(){
@@ -26,11 +12,25 @@ $(document).ready(function(){
     })
     $(".fancy-button2").click(function(){
         $(".toggle-on-click").show()
+    })/////MAIN BUTTON
+
+
+    $(".first-question").click(function(){
+        $(".first-answer").toggle(300)
     })
+    $(".first-question1").click(function(){
+        $(".first-answer1").toggle(300)
+
+    })
+    $(".first-question2").click(function(){
+        $(".first-answer2").toggle(300)
+    })
+    $(".first-question3").click(function(){
+        $(".first-answer3").toggle(300)
+    })//////FAQ TOGGLE CLASS
 
 
-
-
+    
 });
 
 
